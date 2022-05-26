70cc9c93be52fec51bc6bfb3739057c03427ebc3
gumingWu <15626185054@163.com>
2022-05-26 23:41:48 +0800

    test commit

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ const fs = require("fs");
 const id = (~~(Math.random() * 100000)).toString(); // 获取小于10w的数字
 const url = `https://robohash.org/${id}`;
 const dirPath = path.resolve(__dirname, "pic");
-const now = new Date().toLocaleDateString().replaceAll("/", "-");
-request(url).pipe(fs.createWriteStream(`${dirPath}/${now}.png`));
+const date = new Date().toLocaleDateString().replaceAll("/", "-");
+const timestamp = new Date()
+  .toLocaleTimeString(undefined, {
+    hour12: false,
+  })
+  .replaceAll(":", "-");
+request(url).pipe(fs.createWriteStream(`${dirPath}/${date}-${timestamp}.png`));
