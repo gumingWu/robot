c6d1ddbd69b14c024edf47d9f1d956c80923b605
gumingWu <15626185054@163.com>
2022-05-29 12:11:11 +0800

    修改逻辑

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ const fs = require("fs");
 const id = (~~(Math.random() * 100000)).toString(); // 获取小于10w的数字
 const url = `https://robohash.org/${id}`;
 const dirPath = path.resolve(__dirname, "pic");
-const date = new Date().toLocaleDateString("cn").replaceAll("/", "-");
-const timestamp = new Date()
-  .toLocaleTimeString("cn", {
-    hour12: false,
-  })
-  .replaceAll(":", "-");
-request(url).pipe(fs.createWriteStream(`${dirPath}/${date}-${timestamp}.png`));
+// 这一步的处理，因为github获取的时间时区是美国时区，所以获取到的时间格式是5/28/2022，我们可以先split后将年份放到数组第一位，这样就是需要的文件名格式了
+const dateArr = new Date().toLocaleDateString().split("/"); // 本地调试时用.toLocaleDateString("en")
+dateArr.unshift(dateArr.pop());
+const date = dateArr.join("-");
+request(url).pipe(fs.createWriteStream(`${dirPath}/${date}.png`));
